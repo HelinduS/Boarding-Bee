5d25476804c4ba857493e761f9a6a15558221872
HelinduS <helindusenadheera@gmail.com>
2025-09-05 17:50:12 +0530

    Add UsersController and User model for user management
    - Implemented UsersController with endpoints to get all users, get a user by ID, and create a new user.
    - Created User model with properties for Id, Username, Email, PasswordHash, CreatedAt, PhoneNumber, and Role.
    - Added Entity Framework Core targets for project metadata.

--- BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
+++ BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94dedca5cf4ab5d525222f419cf7e55baf4fc87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd55d7c433f7fbd4af22aefbac551c637694e7a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
