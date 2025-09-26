f74c5376a8049670858c5870d0bca9dedb4dd405
HelinduS <helindusenadheera@gmail.com>
2025-09-26 21:07:52 +0530

    fix: update AssemblyInfo.cs and AssemblyInfoInputs.cache for versioning consistency

--- BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
+++ BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
@@ -13,8 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6bb7db2320cdd75ed62e4cc135a1d08c5b521c")]
-
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53d8e277db5846a87c0b3bfbaf82c76b0b763fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
