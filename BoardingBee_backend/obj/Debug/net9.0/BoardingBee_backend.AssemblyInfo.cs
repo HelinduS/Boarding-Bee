af13483e1463004e14eac22ccfcc69727609d56d
HelinduS <helindusenadheera@gmail.com>
2025-09-26 21:10:01 +0530

    update

--- BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
+++ BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
@@ -13,7 +13,9 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74c5376a8049670858c5870d0bca9dedb4dd405")]
+
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6bb7db2320cdd75ed62e4cc135a1d08c5b521c")]
+
 [assembly: System.Reflection.AssemblyProductAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
