f96e9789581d413e6d654f52930644dca8842574
IT23398184 <IT23398184@my.sliit.lk>
2025-10-19 21:46:21 +0530

    jmeter: fix e2e.jmx ResultCollector for non-gui; add jmeter plan and users.csv; backend notification resilience and frontend test-hooks

--- BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
+++ BoardingBee_backend/obj/Debug/net9.0/BoardingBee_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa86dbe6487f4ad35799c40ed5feec26d15b49d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2027324bb6ef6bd138ad48d5c8d08837689dad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardingBee_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
